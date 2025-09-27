787a3c593c5bc3b088643cd32b932641a430cd07
Tyler Johnson <tyler@tylerjohnsondesign.com>
2025-09-27 16:49:36 -0400

    (main) ✨ Header updates to old style

--- header.php
+++ header.php
@@ -28,6 +28,57 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'test' ); ?></a>
 
 	<header id="masthead" class="site-header">
+		<div class="header__desktop" style="background:url('<?php echo SOL_URL . 'assets/newtopbg.png'; ?>') no-repeat; background-position: center top;">
+			<div class="header__desktop-wrapper">
+				<div class="header__desktop-donate">
+					<a href="https://sonsofliberty.dntly.com/#/" class="sol-donate-btn" target="_blank">
+						<img src="<?php echo SOL_URL . 'assets/donate_globe.png'; ?>" alt="Donations Gratefully Received" />
+						<span>Donations</span> Gratefully Received
+					</a>
+				</div>
+				<div class="newsletter-signup">
+					<a href="<?php echo site_url( '/newsletter' ); ?>">
+						Sign Up Here for<br>Email Updates
+					</a>
+				</div>
+				<div class="listen-live-gcn">
+					<a href="https://www2.gcnlive.com/JW1D/index.php/onair?show=39&type=onDem" target="_blank" rel="noopener">
+						<?php include SOL_PATH . 'assets/radio.svg'; ?>
+						Listen Live at GCN
+					</a>
+				</div>
+				<div id="desktop-site-social"><?php
+
+					// Loop.
+					foreach( get_field( 'social_networks', 'options' ) as $network ) { 
+
+						// Output. ?>
+						<a href="<?php echo $network['link']; ?>" target="_blank" rel="noopener">
+							<?php include SOL_PATH . 'assets/' . $network['network'] . '.svg'; ?>
+						</a><?php
+
+					} ?>
+
+				</div>
+				<div class="header__desktop-ycr" style="background:url('<?php echo SOL_URL . 'assets/ycr.png'; ?>') no-repeat;"></div>
+			</div>
+			<div class="header__desktop-menu">
+				<nav class="main-navigation">
+					<div class="header-nav"><?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'main-menu',
+								'menu_id'        => 'main-menu',
+							)
+						); ?>
+						<a href="https://sonsofliberty.dntly.com/#/" class="sol-donate-btn" target="_blank">Donate</a>
+						<a href="/" class="sol-search-btn">
+							<?php include SOL_PATH . 'assets/search.svg'; ?>
+						</a>
+					</div>
+				</nav>
+			</div>
+		</div>
 		<div class="header__inner" style="background:url('<?php echo SOL_URL . 'assets/background.jpg'; ?>') no-repeat;">
 			<div class="container container-flex">
 				<div id="site-logo">
